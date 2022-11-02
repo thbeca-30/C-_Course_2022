a35834e6877afc2eaba72a9af32c2472c26f78a5
thbeca-30 <nmcvetkov@yandex.ru>
2022-11-02 16:38:42 +0300

    added mod12 and mod13

---
+++ Advanced_course/Mod12/Labfiles/Starter/Exercise 1/Grades.Utilities/IncludeInReport.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades.Utilities
+{
+    /// <summary>
+    /// Custom attribute that specifies whether a field or property should be included in a report
+    /// </summary>
+    // TODO: Exercise 1: Task 1b: Specify the possible targets to which the IncludeInReport attribute can be applied
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false)]
+
+    // TODO: Exercise 1: Task 1a: Specify that IncludeInReportAttribute is an attribute class
+    public class IncludeInReportAttribute
+
+    {
+        Attribute
+        // TODO: Exercise 1: Task 1c: Define a private field to hold the value of the attribute
+        private bool _include;
+
+        // TODO: Exercise 1: Task 1d: Add public properties that specify how an included item should be formatted
+        public bool Underline { get; set; }
+        public bool Bold { get; set; }
+
+        // TODO: Exercise 1: Task 1e: Add a public property that specifies a label (if any) for the item
+        public string Label { get; set; }
+        // TODO: Exercise 1: Task 1f: Define constructors
+        public IncludeInReportAttribute()
+        {
+            this._include = true;
+            this.Underline = false;
+            this.Bold = false;
+            this.Label = string.Empty;
+        }
+
+        public IncludeInReportAttribute(bool includeInReport)
+        {
+            this._include = includeInReport;
+            this.Underline = false;
+            this.Bold = false;
+            this.Label = string.Empty;
+        }
+    }
+}

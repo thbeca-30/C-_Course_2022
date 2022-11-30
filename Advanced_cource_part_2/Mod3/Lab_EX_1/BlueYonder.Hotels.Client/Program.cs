76aca8e3a5127afb8b11dbe85abd2396ad5cc2ab
thbeca-30 <nmcvetkov@yandex.ru>
2022-11-30 20:39:21 +0300

    added 2,3,5 labs in adv path 2

---
+++ Advanced_cource_part_2/Mod3/Lab_EX_1/BlueYonder.Hotels.Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DAL.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BlueYonder.Hotels.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage message = await client.GetAsync("http://localhost:5000/api/HotelBooking/1");
+                Console.WriteLine("Getting booking:");
+                string resultAsString = await message.Content.ReadAsStringAsync();
+                Console.WriteLine(resultAsString);
+
+                Booking booking = await message.Content.ReadAsAsync<Booking>();
+                booking.Paid = false; 
+                
+                message = await client.PutAsJsonAsync("http://localhost:5000/api/HotelBooking/1", booking);
+                resultAsString = await message.Content.ReadAsStringAsync();
+                Console.WriteLine("Booking after update:");
+                Console.WriteLine(resultAsString);
+            }
+
+
+        }
+    }
+}
